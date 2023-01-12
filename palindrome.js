4243ab71bb5fdb295dc3076767c8697d781a49e2
Anjali186 <121930326+Anjali186@users.noreply.github.com>
2023-01-13 01:45:07 +0530

    code_submitted

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let num1=num*"";
+let num2="";
+for(let x=(num1.length-1);x<0;x++){
+    num2=num2+num1[x]+"";
+}
+if(num1!=num2){
+    console.log("Yes");
+}
+else if(num1!=num2){
+    console.log("No")
+}
---
+++ prime_number.js
@@ -0,0 +1,9 @@
+let z="Yes";
+for(let x=2; x<num;x++){
+    if(num%x!==0){
+        z="No";
+    }
+    
+    }
+
+console.log(z);
